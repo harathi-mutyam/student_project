01032d04b46dff6b81d6c39598e5d62874e44dc8
harathi-mutyam <ehmutyam@gmail.com>
2026-05-12 17:00:26 +0200

    Update search.php

--- php-app/search.php
+++ php-app/search.php
@@ -1,31 +1,46 @@
-<?php include 'config.php';
+<?php
+include 'config.php';
 
-$search = $_GET['search'];
+$search = $_GET['search'] ?? '';
 
-$result = mysqli_query($conn,
-"SELECT * FROM students WHERE name LIKE '%$search%'");
+$result = mysqli_query($conn, "SELECT * FROM students WHERE name LIKE '%$search%'");
 ?>
 
-<h2>Search Results</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Results - Student Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-<a href="index.php">Back</a>
+<h2>Search Results</h2>
+<a href="students.php">⬅ Back</a>
 
 <table border="1">
   <tr>
-<th>ID</th>
-<th>Name</th>
-<th>Email</th>
-</tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+  </tr>
 
-
-<?php while($row = mysqli_fetch_assoc($result)) { ?>
-
-<tr>
-<td><?php echo $row['id']; ?></td>
-<td><?php echo $row['name']; ?></td>
-<td><?php echo $row['email']; ?></td>
-</tr>
-
-<?php } ?>
+<?php
+if(mysqli_num_rows($result) > 0) {
+    // Loop through results
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>".$row['id']."</td>";
+        echo "<td>".$row['name']."</td>";
+        echo "<td>".$row['email']."</td>";
+        echo "</tr>";
+    }
+} else {
+    // No results found
+    echo "<tr><td colspan='3' style='text-align:center;'>No students found</td></tr>";
+}
+?>
 
 </table>
+
+</body>
+</html>

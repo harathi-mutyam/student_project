246d60fd9f3963f85e34ebe63aa79ad0c993f3db
harathi-mutyam <ehmutyam@gmail.com>
2026-05-12 18:23:55 +0200

    Create save_student.php

---
+++ php-app/save_student.php
@@ -0,0 +1,21 @@
+<?php
+include 'config.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$stmt = $conn->prepare("INSERT INTO students (name, email) VALUES (?, ?)");
+$stmt->bind_param("ss", $name, $email);
+
+if ($stmt->execute()) {
+        #echo "Student Added Successfully<br><br>";
+        #echo '<a href="students.php">⬅ Go back</a>';
+        header("Location: index.php");
+        exit();
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$stmt->close();
+$conn->close();
+?>

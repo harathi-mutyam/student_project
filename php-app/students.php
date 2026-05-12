7c5dbdb4f5eca1ce6f5f5f79ff0eb84220155b5d
harathi-mutyam <ehmutyam@gmail.com>
2026-05-12 16:27:10 +0200

    Update students.php

--- php-app/students.php
+++ php-app/students.php
@@ -1,39 +1,62 @@
 <?php
 include 'config.php';
-
-$result = mysqli_query($conn, "SELECT * FROM students");
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Students List</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-<h1>Student Details</h1>
+<h1>Student Management System</h1>
 
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Email</th>
-    </tr>
-
-    <?php while($row = mysqli_fetch_assoc($result)) { ?>
-    <tr>
-        <td><?php echo $row['id']; ?></td>
-        <td><?php echo $row['name']; ?></td>
-        <td><?php echo $row['email']; ?></td>
-    </tr>
-    <?php } ?>
-
-</table>
-
-<div class="add-link">
+<!-- Add Student Button -->
+<div style="text-align:center; margin-bottom:20px;">
     <a href="index.php">➕ Add Student</a>
 </div>
+
+<!-- Search Form -->
+<form action="search.php" method="GET">
+    <input type="text" name="search" placeholder="Search student by Name">
+    <button type="submit">Search</button>
+</form>
+
+<br>
+
+<table border="1">
+
+<tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Action</th>
+</tr>
+
+<?php
+$result = mysqli_query($conn, "SELECT * FROM students");
+
+while($row = mysqli_fetch_assoc($result)) {
+?>
+
+<tr>
+    <td><?php echo $row['id']; ?></td>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['email']; ?></td>
+
+    <td>
+        <a href="delete_student.php?id=<?php echo $row['id']; ?>"
+           onclick="return confirm('Are you sure you want to delete this student?');">
+            Delete
+        </a>
+    </td>
+</tr>
+
+<?php } ?>
+
+</table>
 
 </body>
 </html>

a4309fb094626ffb3c1a98d09cd2bb2e689c908b
harathi-mutyam <ehmutyam@gmail.com>
2026-05-12 13:18:23 +0200

    Update add_student.php

--- php-app/add_student.php
+++ php-app/add_student.php
@@ -4,10 +4,16 @@ include 'config.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-$sql = "INSERT INTO students(name,email)
-VALUES('$name','$email')";
+$stmt = $conn->prepare("INSERT INTO students (name, email) VALUES (?, ?)");
+$stmt->bind_param("ss", $name, $email);
 
-mysqli_query($conn, $sql);
+if ($stmt->execute()) {
+    echo "Student Added Successfully<br><br>";
+    echo '<a href="students.php">⬅ Go back</a>';
+} else {
+    echo "Error: " . $conn->error;
+}
 
-echo "Student Added";
+$stmt->close();
+$conn->close();
 ?>

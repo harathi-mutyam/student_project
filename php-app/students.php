55f25e02edf46695f3069505627f6cf9be784278
harathi-mutyam <ehmutyam@gmail.com>
2026-05-12 13:57:18 +0200

    Update students.php

--- php-app/students.php
+++ php-app/students.php
@@ -8,32 +8,11 @@ $result = mysqli_query($conn, "SELECT * FROM students");
 <html>
 <head>
     <title>Students List</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 70%;
-            margin: 20px auto;
-        }
-
-        th, td {
-            border: 1px solid #333;
-            padding: 10px;
-            text-align: center;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-
-        .add-link {
-            text-align: center;
-            margin-top: 20px;
-        }
-    </style>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
-<h2 style="text-align:center;">Student Details</h2>
+<h1>Student Details</h1>
 
 <table>
     <tr>
@@ -43,17 +22,17 @@ $result = mysqli_query($conn, "SELECT * FROM students");
     </tr>
 
     <?php while($row = mysqli_fetch_assoc($result)) { ?>
-        <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['name']; ?></td>
-            <td><?php echo $row['email']; ?></td>
-        </tr>
+    <tr>
+        <td><?php echo $row['id']; ?></td>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['email']; ?></td>
+    </tr>
     <?php } ?>
 
 </table>
 
 <div class="add-link">
-    <a href="add_student_form.php">➕ Add Student</a>
+    <a href="index.php">➕ Add Student</a>
 </div>
 
 </body>

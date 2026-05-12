3a2406746a6391798088fb171ce9aea69122b836
harathi-mutyam <ehmutyam@gmail.com>
2026-05-12 16:18:29 +0200

    Update add_student.php

--- php-app/add_student.php
+++ php-app/add_student.php
@@ -8,8 +8,10 @@ $stmt = $conn->prepare("INSERT INTO students (name, email) VALUES (?, ?)");
 $stmt->bind_param("ss", $name, $email);
 
 if ($stmt->execute()) {
-    echo "Student Added Successfully<br><br>";
-    echo '<a href="students.php">⬅ Go back</a>';
+        #echo "Student Added Successfully<br><br>";
+        #echo '<a href="students.php">⬅ Go back</a>';
+        header("Location: students.php");
+        exit();
 } else {
     echo "Error: " . $conn->error;
 }

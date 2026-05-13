08f041257f527bf10595188273b3d2a6a0889753
harathi-mutyam <ehmutyam@gmail.com>
2026-05-13 11:57:23 +0200

    Update login.php

--- php-app/login.php
+++ php-app/login.php
@@ -1,65 +1,6 @@
 <?php
 session_start();
-include 'config.php';
+session_destroy();
 
-if (isset($_POST['login'])) {
-
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = $_POST['password'];
-
-    $query = "SELECT * FROM users WHERE email='$email'";
-    $result = mysqli_query($conn, $query);
-
-    if ($result && mysqli_num_rows($result) == 1) {
-
-        $user = mysqli_fetch_assoc($result);
-
-        if (password_verify($password, $user['password'])) {
-
-            // COMMON SESSION DATA
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['email'] = $user['email'];
-            $_SESSION['role'] = $user['role'];
-
-            // ROLE BASED SESSION
-            if ($user['role'] == 'student') {
-                $_SESSION['student_id'] = $user['student_id'];
-                header("Location: student_dashboard.php");
-                exit();
-            }
-
-            if ($user['role'] == 'admin') {
-                header("Location: admin_dashboard.php");
-                exit();
-            }
-
-        } else {
-            $error = "Invalid password";
-        }
-
-    } else {
-        $error = "User not found";
-    }
-}
-?>
-
-<!-- SIMPLE LOGIN FORM -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-
-<h2>Login</h2>
-
-<?php if (isset($error)) echo "<p style='color:red'>$error</p>"; ?>
-
-<form method="POST">
-    <input type="email" name="email" placeholder="Email" required><br><br>
-    <input type="password" name="password" placeholder="Password" required><br><br>
-    <button type="submit" name="login">Login</button>
-</form>
-
-</body>
-</html>
+header("Location: login.php");
+exit();

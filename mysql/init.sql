3467c6e46732acc38264935de753aabfd0dc5e2b
harathi-mutyam <ehmutyam@gmail.com>
2026-05-13 11:06:17 +0200

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -19,7 +19,8 @@ CREATE TABLE users (
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('admin','student') NOT NULL,
-    student_id INT NULL
+    student_id INT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
 );
 CREATE TABLE attendance (
     id INT AUTO_INCREMENT PRIMARY KEY,

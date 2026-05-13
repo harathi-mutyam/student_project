e707297e9b2220b0156cb29993cc39e15be584b8
harathi-mutyam <ehmutyam@gmail.com>
2026-05-13 09:59:15 +0200

    Update student_subjects.php

--- php-app/student_subjects.php
+++ php-app/student_subjects.php
@@ -79,6 +79,7 @@ if (isset($_POST['update'])) {
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>

851dd248b1d77494e3544a60e8558b9d5cd1bbf5
harathi-mutyam <ehmutyam@gmail.com>
2026-05-13 10:58:58 +0200

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -14,3 +14,22 @@ CREATE TABLE subjects (
     marks INT,
     FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
 );
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin','student') NOT NULL,
+    student_id INT NULL
+);
+CREATE TABLE attendance (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    attendance_date DATE,
+    status ENUM('Present','Absent')
+);
+INSERT INTO users(email,password,role)
+VALUES(
+'admin@gmail.com',
+'$2y$10$KbQiQhK7Vx9M0LQ6YV4gW.r7B9QnV8Kx3N0X2R5gT5nQ2M8hK0w0S',
+'admin'
+);
